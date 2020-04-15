b50845aff256ef5068e83e578aa223d345037901
mingyeeee <61096358+mingyeeee@users.noreply.github.com>
2020-04-15 16:10:04 -0400

    Update motionAnalysis.cpp
    this will be the only code running on the pi to monitor the motion of the backpack and do binder motion analysis. uses multithreading

--- motionAnalysis.cpp
+++ motionAnalysis.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+
 #include <stdio.h>
 #include <stdint.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include <cstdlib>
- 
+#include <thread>
+
 #define MPU6050_GYRO_XOUT_H        0x43   // R
 #define MPU6050_GYRO_YOUT_H        0x45   // R
 #define MPU6050_GYRO_ZOUT_H        0x47   // R
@@ -12,21 +18,26 @@
 #define MPU6050_PWR_MGMT_1         0x6B   // R/W
 #define MPU6050_I2C_ADDRESS        0x68   // I2C
 using namespace std;
+
+//shared variables between threads
+static int stop=0;
+static vector<int> piMPU;
+static bool motionDetected = false;
+//------------------mpu monitoring------------------------
 static int fd;
-int read_word_2c(int addr)
-{
-int val;
-val = wiringPiI2CReadReg8(fd, addr);
-val = val << 8;
-val += wiringPiI2CReadReg8(fd, addr+1);
-if (val >= 0x8000)
-val = -(65536 - val);
- 
-return val;
+int read_word_2c(int addr){
+	int val;
+	val = wiringPiI2CReadReg8(fd, addr);
+	val = val << 8;
+	val += wiringPiI2CReadReg8(fd, addr+1);
+	if (val >= 0x8000)
+	val = -(65536 - val);
+	 
+	return val;
 }
 
-int main()
-{
+int motionMonitoring(int (*read_word_2cPTR)(int)){
+    cout << "starting motion monitoring thread" << "\n";
     fd = wiringPiI2CSetup(MPU6050_I2C_ADDRESS);
     if (fd == -1)
         return 1;
@@ -35,7 +46,10 @@ int main()
     wiringPiI2CWriteReg16(fd, MPU6050_PWR_MGMT_1, 0);
  
     int x,y,z;
- 
+    
+    vector<int>::iterator it;
+    int motionDetectedCounter;
+    int vsize;
     while(true)
     {
         x = read_word_2c(0x3B);
@@ -46,19 +60,41 @@ int main()
         y = ((int)y / 1000) + 42;
         z = ((int)z / 1000) + 42;
 
-        cout << x << endl;
+        //cout << x << "\n";
+	piMPU.push_back(x);
+	if(piMPU.size() > 100){
+	    it = piMPU.begin();
+	    piMPU.erase(it);
+	}
+	
+	if(piMPU.size() == 100){
+	    motionDetectedCounter = 0;
+	    vsize = piMPU.size()-1;
+	    for(auto i=0; i<10; i++){
+		if(abs(piMPU.at(vsize - i)-piMPU.at(vsize - (i+1))) >1){
+		    motionDetectedCounter++;
+		    if (motionDetectedCounter > 5){
+			motionDetected = true;
+			//cout << "motion detected" << "\n";
+			
+			break;
+		    }
+		}
+	    }
+	}
+	if(stop ==1){
+	    break;
+	}
+	sleep(0.05);
     }
 }
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <unistd.h>
-
-using namespace std;
-
+//--------------------------stop all thread--------------------------------
+void stopProgram(){
+    //type 1 to stop
+    cout << "stop program thread starting" << endl;
+    cin >> stop;
+}
+//--------------------------binder motion----------------------------------
 struct Binder
 {
     string subject;
@@ -69,21 +105,16 @@ struct Binder
     bool inBag=true;
 };
 
-struct Backpack{
-    int motion[100];
-    string rawmotion;
-};
-
 void getFileContent(string &bindersMotion, string filePath, bool clearFile);
 void fillBinderSubjects(vector<Binder> &binder, string &initializationInfo);
-void fillBinderMotion(vector<Binder> &binder, string &binderMotion);
-bool readIfMotionIsDetected();
-void analysisBinderMotion(vector<Binder> &binder, Backpack &backpack);
-vector<int> checkForBinderUpdates(vector<Binder> binder);
+void analysisBinderMotion(vector<Binder> &binder);
 void requestBinderMotion();
 
-int main() {
-    Backpack backpack;
+int main(){
+    thread mpuMonitoring(motionMonitoring, read_word_2c);
+    thread stopAll(stopProgram);
+    cout << "main thread" << "\n";
+    //initialization for binder stuff
     string initializationInfo;
     string binderInitializationInfo = "/home/pi/iotBinder/binderInitializationInfo.txt";
     int bindersPresent;
@@ -101,25 +132,17 @@ int main() {
     cout << "number of binders present: " << binder.size() << endl;
 
     fillBinderSubjects(binder,initializationInfo);
-
-    bool inMotion;
-    while (true){
-        inMotion = readIfMotionIsDetected();
-        if(inMotion){
-            requestBinderMotion();
-            sleep(5);
-            getFileContent(bindersMotion, biMotion, true);
-
-            cout << bindersMotion << endl;
-            fillBinderMotion(binder,bindersMotion);
-            cout << "starting analysis" << endl;
-            analysisBinderMotion(binder, backpack);
-            cout << "done analysis" << endl;
-
-        }
-
+    while(stop == 0){
+	if(motionDetected){
+	    motionDetected = false;
+	    requestBinderMotion();
+	    sleep(3.5);
+	    analysisBinderMotion(binder);
+	}
     }
-    return 0;
+    
+    stopAll.join();
+    mpuMonitoring.join();
 }
 
 void getFileContent(string &contents, string filePath, bool clearFile){
@@ -141,6 +164,7 @@ void getFileContent(string &contents, string filePath, bool clearFile){
         myfile.close();
     }
 }
+
 //populates binder's subjects from a file
 void fillBinderSubjects(vector<Binder> &binder, string &initializationInfo){
     int binderID;//used as a temporary id for binder objects
@@ -163,78 +187,13 @@ void fillBinderSubjects(vector<Binder> &binder, string &initializationInfo){
         }
     }
 }
-//populates the binder xAxisMotion array from file
-void fillBinderMotion(vector<Binder> &binder, string &bindersMotion){
-    int binderID;
-    string motionVal;
-    for(unsigned int i=0;i<159;i++){
-        if(bindersMotion.at(i)=='B'){
-            binderID = bindersMotion.at(i+1)-'0';
-
-            //add 3 to also account for the '.'
-            for(unsigned int j=i+2;j<159;j++){
-                if(bindersMotion.at(j) == 'B'){
-                    i = j-1;
-                    break;
-                }
-                if(bindersMotion.at(j)==','){
-                    binder.at(binderID-1).xAxisMotion[binder.at(binderID-1).indexer]=stoi(motionVal);
-                    binder.at(binderID-1).indexer++;
-                    motionVal.clear();
-                }
-                if(isdigit(bindersMotion.at(j))){
-                    motionVal += bindersMotion.at(j);
-                }
-            }
-        }
-    }
-}
-//read file which mpu.c writes if motion is detected
-bool readIfMotionIsDetected(){
-    bool motionDetected = false;
-    string filePath = "/home/pi/iotBinder/isMotionDetected.txt";
-    string content;
-    getFileContent(content, filePath, true);
-    if(content.compare("1") == 0){
-        motionDetected = true;
-    }
-    return motionDetected;
-}
 
 //updates binder struct boolean inBag
-void analysisBinderMotion(vector<Binder> &binder, Backpack &backpack){
-    cout << "analysis function" << endl;
-    cout << "file" << endl;
-    int tempindexer=0;
-    cout << "geting mpudata" << endl;
-    
-    ofstream cppRequestRead;
-    cppRequestRead.open ("/home/pi/iotBinder/cppRequestRead.txt",ios::out | ios::trunc);
-    cppRequestRead << "1";
-    cppRequestRead.close();
-    
-    //--------------------------------------
-    string line;
-    ifstream myfile ("/home/pi/iotBinder/mpuXData.txt");
-    if (myfile.is_open()){
-        while(getline(myfile,line)){
-            //cout << line << '\n';
-            backpack.rawmotion +=line;
+void analysisBinderMotion(vector<Binder> &binder){
+    //deep copy
+    vector<int> piMPUsnapshot(piMPU);
+    cout << "analysis function" << "\n";
 
-        }
-        myfile.close();
-    }
-    else cout<<"unable to open" << endl;
-    cout << "mpuData " << backpack.rawmotion << endl;
-    //populatint array with file content
-    for(unsigned int j=0; j<backpack.rawmotion.length(); j+=2){
-
-        backpack.motion[j/2]= (backpack.rawmotion.at(j)-'0')*10+(backpack.rawmotion.at(j+1)-'0');
-        cout<< "piMotion " << backpack.motion[j/2]<<endl;
-        tempindexer++;
-
-    }
-    cout<< "indexer: "<< tempindexer << endl;
     int withinThreshold;
     int threshold = 5;
     int matchingThreshold = 10;
@@ -243,14 +202,14 @@ void analysisBinderMotion(vector<Binder> &binder, Backpack &backpack){
         for(auto i=0; i<50; i++){
             withinThreshold = 0;
             for(auto j=i; j<i+50; j++){
-                if(abs(binder.at(b).xAxisMotion[j]-backpack.motion[j])<threshold){
+                if(abs(binder.at(b).xAxisMotion[j]-piMPUsnapshot.at(j))<threshold){
                     withinThreshold++;
                 }
             }
-            cout << "similarity test "<< i << ": "<< withinThreshold << endl;
+            cout << "similarity test "<< i << ": "<< withinThreshold << "\n";
             if(withinThreshold >= matchingThreshold){
                 binder.at(b).inBag =true;
-                cout << "binder in bag" << endl;
+                cout << "binder in bag" << "\n";
                 tempinbag = true;
                 break;
             }
@@ -268,55 +227,3 @@ void requestBinderMotion(){
     myfile << "yes";
     myfile.close();
 }
-
-//unused for now
-//returns a vector with which binders have been updated
-vector<int> checkForBinderUpdates(vector<Binder> &binder){
-    vector<int> binderIDs;
-    string content;
-    string filePath ="/home/pi/iotBinder/binderUpdate.txt";
-    getFileContent(content, filePath, true);
-    int numOfBinders = content.at(0)-'0';
-    binderIDs.resize(numOfBinders);
-    int binderID, counter;
-    string tempVal;
-
-    for(unsigned int i=1; i< content.length();i++){
-        if(content.at(i)=='B'){
-            binderID = content.at(i+1)-'0';
-            //file is formated - Xpos,Ypos,Zpos,lighting
-            for(unsigned int j=i+2;j<content.length();j++){
-                if(content.at(j)=='B'){
-                    counter=0;
-                    i =j-1;
-                    break;
-                }
-                if(content.at(j)==','){
-                    switch (counter){
-                        case 0:
-                            binder.at(binderID-1).xPos=stoi(tempVal);
-                            break;
-                        case 1:
-                            binder.at(binderID-1).yPos=stoi(tempVal);
-                            break;
-                        case 2:
-                            binder.at(binderID-1).zPos=stoi(tempVal);
-                            break;
-                        case 3:
-                            binder.at(binderID-1).lightingVal=stoi(tempVal);
-                            break;
-                        default:
-                            break;
-                    }
-                    counter++;
-                }
-                if(isdigit(content.at(j))){
-                    tempVal += content.at(j);
-                }
-            }
-        }
-    }
-    return binderIDs;
-}
-
-
